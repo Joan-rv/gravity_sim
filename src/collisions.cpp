b900bf69ad448526e2314b0a4b375255a771df18
Joan Ripoll <Joan-rv@users.noreply.github.com>
2023-10-31 17:45:14 +0100

    Implement collision velocity constraint solver

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -1,5 +1,6 @@
 #include "math.hpp"
 #include <SFML/System/Vector2.hpp>
+#include <algorithm>
 #include <cstdlib>
 #include <point.hpp>
 #include <vector>
@@ -18,7 +19,41 @@ void position_contraints(std::vector<Collision>& collisions) {
     }
 }
 
-void velocity_contraints(std::vector<Collision>& collisions) {}
+void velocity_contraints(std::vector<Collision>& collisions) {
+    for (auto& collision : collisions) {
+        float inv_mass1 = 1 / collision.p1.mass;
+        float inv_mass2 = 1 / collision.p2.mass;
+        sf::Vector2f relative_vel =
+            collision.p2.velocity - collision.p1.velocity;
+        float impulse = -2.0f * vec::dot(relative_vel, collision.normal) /
+                        (inv_mass1 + inv_mass2);
+        impulse = -std::fmax(impulse, 0);
+
+        collision.p1.velocity += inv_mass1 * impulse * collision.normal;
+        collision.p2.velocity -= inv_mass2 * impulse * collision.normal;
+    }
+    /*
+    for (auto& collision : collisions) {
+        // Calculate relative velocity of p1 and p2 along the collision normal
+        sf::Vector2f relativeVelocity =
+            collision.p1.velocity - collision.p2.velocity;
+        float relativeSpeed = vec::dot(relativeVelocity, collision.normal);
+
+        // Check if objects are moving toward each other
+        if (relativeSpeed > 0) {
+            // Calculate the impulse (change in momentum) required to resolve
+            // the collision
+            float impulse = -2.0f * relativeSpeed /
+                            (1 / collision.p1.mass + 1 / collision.p2.mass);
+
+            // Update velocities of p1 and p2 based on the impulse and normal
+            sf::Vector2f impulseVector = impulse * collision.normal;
+            collision.p1.velocity += (1 / collision.p1.mass) * impulseVector;
+            collision.p2.velocity -= (1 / collision.p2.mass) * impulseVector;
+        }
+    }
+    */
+}
 
 void handle_collisions(std::vector<Point>& points) {
     std::vector<Collision> collisions;
